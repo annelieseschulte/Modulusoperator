248bf76634ff933f5d62fc26146fa8c1816220f5
anneliese schulte <anneliese.schulte-hordelhoff.2022@student.lhschools.org>
2020-03-06 14:09:10 -0500

    Modulus operator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,17 +10,40 @@ public class Main {
         // testOddness
 
 
-        String positiveNumber;
+        int positiveNumber;
         Scanner keyboard;
         keyboard = new Scanner(System.in);
         int number = 17;
         System.out.println("positive number");
-        positiveNumber = keyboard.nextLine();
+        positiveNumber = keyboard.nextInt();
         System.out.println("Number is even: "+(number % 2 ==0));
         System.out.println("Number is odd: "+(number % 2 !=0));
 
+        //Keep Contained
+        keyboard = new Scanner(System.in);
 
-;
+
+        System.out.println("give a positive number");
+        positiveNumber = keyboard.nextInt();
+        System.out.println("The number contained is"+((positiveNumber%8)+5));
+
+        //Last Digit
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Give a number that is 4 digits long");
+        positiveNumber = keyboard.nextInt();
+        System.out.println("The last digit of the number is "+((positiveNumber%10)+0));
+
+
+
+
+
+
+
+
+
+
+        ;
 
     }
 
